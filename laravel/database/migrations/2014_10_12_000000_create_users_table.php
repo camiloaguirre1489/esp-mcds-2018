8096ca20af89f15f90c81986b242cbb066121717
camiloaguirre1489 <camilo.aguirre1489@gmail.com>
2018-10-09 21:05:33 -0500

    modificacion instalara 09 octubre

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('fullname');
-            $table->string('nickname');
+            $table->string('nickname', 64)->unique();
             $table->string('email',128)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('photo')->default('imagenes/photo.png');
